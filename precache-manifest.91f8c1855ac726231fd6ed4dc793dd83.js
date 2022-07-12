42c8df20e6508cbe82b428e6c18cc7bb9bc0fd2e
Rodrigo Torres <torresperez12@hotmail.com>
2022-07-11 17:41:06 -0700

    Updates

---
+++ precache-manifest.91f8c1855ac726231fd6ed4dc793dd83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cc7f2ed5682b923acba",
+    "url": "/advice-trip-app/static/css/main.018565ae.chunk.css"
+  },
+  {
+    "revision": "6cc7f2ed5682b923acba",
+    "url": "/advice-trip-app/static/js/main.edfd5e3e.chunk.js"
+  },
+  {
+    "revision": "cd7264e2461732f0cb63",
+    "url": "/advice-trip-app/static/js/runtime~main.65d43ef4.js"
+  },
+  {
+    "revision": "1ab71f3fab90844f5630",
+    "url": "/advice-trip-app/static/js/2.39f4f457.chunk.js"
+  },
+  {
+    "revision": "2dacb9c4a29e5ea189e420368ea876d1",
+    "url": "/advice-trip-app/index.html"
+  }
+];
